366c96db4d95c541021954da755b3daaa4821c21
mlee <darum.mlee@gmail.com>
2021-10-21 18:26:17 +0900

    sub07 + sub_08 add

--- markup/assets/js/common.js
+++ markup/assets/js/common.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
-
+    
+    $(".lnb").each(function(){
+        var $this = $(this);
+        $this.find("li").each(function(){
+            $(this).children("a.on").next("ul").show();
+            if($(this).children("ul").length){
+               $(this).children("a").addClass("fold"); 
+            }
+            $(this).children("a").on("click",function(){
+                if($(this).hasClass("on")){
+                    $(this).next("ul").stop().slideUp(200);
+                    $(this).removeClass("on");
+                }
+                else{
+                    $(this).parent().siblings().find("ul").slideUp(200);
+                    $(this).next("ul").stop().slideDown(200);
+                    $(this).parent().siblings().find("a").removeClass("on");
+                    $(this).addClass("on");
+                }
+            })
+        })
+    })
 });
 
 $(window).scroll(function(){
